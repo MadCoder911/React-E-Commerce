6f8899c3314b8b7efb1a13058d6267a24388c538
Royce119 <madcoder911@gmail.com>
2023-02-28 01:54:43 +0200

    initial commit

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="seciton section-center">
+      <div className="loading"></div>
+    </div>
+  );
+};
+
+export default Loading;
